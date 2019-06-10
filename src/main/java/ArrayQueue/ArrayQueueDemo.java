e24ec5e90966d5396adce6c7dc9eeae996b6ffc0
EricWord <2635863525@qq.com>
2019-06-10 10:16:20 +0800

    使用数组来实现队已经实现

--- src/main/java/ArrayQueue/ArrayQueueDemo.java
+++ src/main/java/ArrayQueue/ArrayQueueDemo.java
@@ -1,5 +1,7 @@
 package ArrayQueue;
 
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+
 /**
  * @Description
  * @Author eric
@@ -7,4 +9,125 @@ package ArrayQueue;
  * @Date 2019/6/10
  */
 public class ArrayQueueDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
+
+
+//使用数组来模拟队列
+class ArrayQueue {
+    //数组最大容量
+    private int maxSize;
+    //队列头
+    private int front;
+    //队列尾
+    private int rear;
+    //用于存放数据，模拟队列
+    private int[] arr;
+    //队列的构造器
+
+    public ArrayQueue(int maxSize) {
+        this.maxSize = maxSize;
+        arr = new int[maxSize];
+        //指向队列头的前一个位置
+        front = -1;
+        //指向队列尾的具体数据，就是队列的最后一个数据
+        rear = -1;
+
+    }
+
+    /**
+     * 判断队列是否已满
+     *
+     * @return ture :已满
+     */
+    public boolean isFull() {
+        return rear == maxSize - 1;
+    }
+
+    /**
+     * 判断队列是否为空
+     *
+     * @return true:队列为空
+     */
+    public boolean isEmpty() {
+        return rear == front;
+    }
+
+    /**
+     * 添加数据到队列
+     *
+     * @param n
+     */
+    public void addQueue(int n) {
+        //判断队列是否满
+        if (isFull()) {
+            System.out.println("队列已满，不能再添加数据到队列");
+            //结束添加
+            return;
+
+        }
+        //让尾指针后移
+        rear++;
+        arr[rear] = n;
+
+    }
+
+    /**
+     * 获取队列数据
+     * @return
+     */
+    public int getQueue(){
+        //判断队列是否为空
+        if(isEmpty()){
+            //通过抛出异常
+            throw new RuntimeException("队列空，不能取数据！");
+
+        }
+        //头指针后移
+        front++;
+        //返回头指针所指向的数据
+        return  arr[front];
+
+    }
+
+    /**
+     * 显示队列数据
+     */
+    public void showQueue(){
+        //判断队列是否为空
+        if(isEmpty()){
+            System.out.println("队列为空，没有数据！");
+            //直接结束
+            return;
+
+        }
+        //遍历数组，输出队列中的所有数据
+        for (int i : arr) {
+            System.out.printf("arr[%d]=%d\n",i,arr[i]);
+
+            
+        }
+
+    }
+
+    /**
+     * 显示队列的头部，不是取出头数据
+     * @return
+     */
+    public  int headQueue(){
+
+        //判断队列是否为空
+        if(isEmpty()){
+            System.out.println("队列为空，没有头数据！");
+            throw  new RuntimeException("没有头数据！");
+
+        }
+        return  arr[front+1];
+    }
+
 }
