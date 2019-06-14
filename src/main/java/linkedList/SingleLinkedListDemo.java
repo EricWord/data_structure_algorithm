d4c4a65571e3b7cfebc47a5044ec37786cd11d45
EricWord <2635863525@qq.com>
2019-06-14 17:27:22 +0800

    有关单链表的面试题

--- src/main/java/linkedList/SingleLinkedListDemo.java
+++ src/main/java/linkedList/SingleLinkedListDemo.java
@@ -31,7 +31,83 @@ public class SingleLinkedListDemo {
         singleLikedList.addByOrder(node3);
 
         //显示单链表
+        System.out.println("修改前的链表：");
         singleLikedList.list();
+
+        HeroNode newNode = new HeroNode(2, "卢俊义dvlha", "玉麒麟dsvj");
+        singleLikedList.update(newNode);
+        System.out.println("修改后的链表：");
+        singleLikedList.list();
+
+        singleLikedList.del(1);
+        System.out.println("删除某个节点后的链表：");
+        singleLikedList.list();
+
+
+        //输出链表有有效节点的个数
+        System.out.println(getLength(singleLikedList.getHead()));
+
+    }
+
+
+    /**
+     * 获取单链表中有效节点的个数(如果是带头节点的单链表，要把头节点排除在外)
+     *
+     * @param head 链表的头节点
+     * @return 有效节点个数
+     */
+    public static int getLength(HeroNode head) {
+
+        //判断是否是空链表
+        if (head.next == null) {
+            return 0;
+
+        }
+
+        int length = 0;
+        HeroNode cur = head.next;
+        while (cur != null) {
+            length++;
+            //遍历
+            cur = cur.next;
+
+
+        }
+
+        return length;
+
+    }
+
+
+    //查找单链表中的倒数第k个节点
+    //编写一个方法接收head节点，同时接收一个Index
+    //index表示倒数第index个节点
+    //先把链表从头到尾遍历一遍，得到链表的总长度
+    //得到链表的总长度后，从链表的第一个节点开始遍历，遍历到(length-index)个节点
+    public static HeroNode findLastIndexNode(HeroNode head, int index) {
+        //链表为空，直接返回Null
+        if (head.next == null) {
+            return null;
+
+        }
+
+        //第一次遍历得到链表的长度
+        int size = getLength(head);
+        //第二次遍历到(size-index)位置，就是倒数第K个节点的位置
+        //校验index
+        if (index <= 0 || index > size) {
+            return null;
+
+        }
+
+        HeroNode cur = head.next;
+        for (int i = 0; i < size - index; i++) {
+            cur = cur.next;
+
+        }
+
+
+        return cur;
 
     }
 
@@ -60,7 +136,6 @@ class HeroNode {
                 "no=" + no +
                 ", name='" + name + '\'' +
                 ", nickName='" + nickName + '\'' +
-                ", next=" + next +
                 '}';
     }
 }
@@ -71,6 +146,9 @@ class SingleLikedList {
     //先初始化一个头节点，头节点不要动，不存放具体的数据
     private HeroNode head = new HeroNode(0, "", "");
 
+    public HeroNode getHead() {
+        return head;
+    }
 
     /**
      * 添加节点到单向链表
@@ -149,6 +227,90 @@ class SingleLikedList {
     }
 
 
+    /**
+     * 修改节点的信息，根据编号来修改，即编号不能修改
+     * 根据newNode的节点信息修改即可
+     *
+     * @param newNode
+     */
+    public void update(HeroNode newNode) {
+        //判断是否为空
+        if (head.next == null) {
+            System.out.println("链表为空！");
+            return;
+
+        }
+        //不为空，根据no来找到需要修改的节点
+
+        HeroNode temp = head.next;
+        //表示是否找到该节点
+        boolean flag = false;//默认没有找到
+        while (true) {
+            if (temp == null) {
+                //到达链表的结尾,链表遍历结束
+                break;
+            }
+
+            if (temp.no == newNode.no) {
+                //找到了要修改的节点
+                flag = true;
+                break;
+            }
+
+            //没找到继续找
+            temp = temp.next;
+
+        }
+
+        //根据flag判断是否找到
+        if (flag) {
+            temp.name = newNode.name;
+            temp.nickName = newNode.name;
+        } else {
+            System.out.printf("没有找到编号为%d的节点", newNode.no);
+
+        }
+
+
+    }
+
+
+    //删除节点
+    //思路：
+    //1.找到要删除节点的前一个节点
+    public void del(int no) {
+        HeroNode temp = head;
+        //标识是否找到待删除的节点
+        boolean flag = false;
+        while (true) {
+            //已经到链表的最后
+            if (temp.next == null) {
+                break;
+
+            }
+            if (temp.next.no == no) {
+                //找到的待删除节点的前一个节点temp
+                flag = true;
+                break;
+
+            }
+            //没找到继续遍历
+            temp = temp.next;
+        }
+
+        //循环结束后根据flag来判断是否已经找到
+        if (flag) {
+            //已经找到，删除
+            temp.next = temp.next.next;
+
+        } else {
+            System.out.println("要删除的节点不存在！");
+        }
+
+
+    }
+
+
     //显示链表
     public void list() {
         //判断链表是否为空
@@ -166,7 +328,7 @@ class SingleLikedList {
                 break;
             }
             //输出节点信息
-            System.out.println(temp);
+            System.out.println(temp.toString());
             //当前指针后移
             temp = temp.next;
 
