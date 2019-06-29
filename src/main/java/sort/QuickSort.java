c98af41a5efcc53132b0b8b1d5243940cfe65f79
EricWord <2635863525@qq.com>
2019-06-29 08:17:39 +0800

    快速排序思路分析

---
+++ src/main/java/sort/QuickSort.java
@@ -0,0 +1,27 @@
+package sort;
+
+/**
+ * @Description 快速排序
+ * @Author eric
+ * @Version V1.0.0
+ * @Date 2019/6/29
+ */
+public class QuickSort {
+    /**
+     * 快速排序的思路：
+     * 通过一趟排序将要排序的数据分割成两个独立的部分，
+     * 其中一部分的所有数据都比另外一部分的所有数据都要小
+     * 然后再按照此方法对这两部分数据分别进行快速排序
+     * 整个排序过程可以递归进行
+     * 以此达到整个数据变为有序列
+     *
+     */
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
