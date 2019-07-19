137b8c2469e5e34572bfb310dfb39117a0a06fb4
EricWorlder <2635863525@qq.com>
2019-07-19 11:36:31 +0800

    树结构的代码实现之数组底层扩容机制

---
+++ src/main/java/tree/TestTree.java
@@ -0,0 +1,23 @@
+package tree;
+
+import java.util.ArrayList;
+
+/**
+ * @Description 树的测试
+ * @Author eric
+ * @Version V1.0.0
+ * @Date 2019/7/19
+ */
+public class TestTree {
+
+
+    public static void main(String[] args) {
+
+        //以ArrayList分析底层是如何实现数组扩容
+        ArrayList<String> strings = new ArrayList<>();
+        //底层维护了一个数组 transient Object[] elementData;
+
+
+
+    }
+}
